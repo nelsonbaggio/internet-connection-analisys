90875ce2d762447ffd913f4961c440480dce331f
Nelson Baggio <nelson.baggio@gmail.com>
2018-11-17 14:22:47 -0200

    improvements

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const ping = require('ping');
 const fs = require('fs');
 
 const hostName = process.argv[2];
-let time = process.argv[3];
+let time = parseInt(process.argv[3]);
+const now = new Date().toISOString();
 
 const pingHost = async (host) => {
-    const fileName = "log-" + new Date().toISOString() + ".json"
+    const fileName = `log-${now}.json`;
     var stream = fs.createWriteStream(fileName, { flags: 'a' });
     setInterval(async () => {
         if (time == 0) {
@@ -13,16 +14,20 @@ const pingHost = async (host) => {
             stream.end();
         }
         const res = await ping.promise.probe(host, { min_reply: 1000 });
-        if (!res.alive) {
-            const log = {
-                time: new Date(),
-                output: res
-            }
-            stream.write(JSON.stringify(log) + "," + "\n");
+        if (res.alive) {
+            const log = buildLog(res);
+            stream.write(`${log},\n`);
         }
-        console.log(time);
         time--;
     }, 1000);
+}
+
+const buildLog = (res) => {
+    const log = {
+        time: new Date(),
+        output: res.output
+    };
+    return JSON.stringify(log);
 }
 
 pingHost(hostName);
